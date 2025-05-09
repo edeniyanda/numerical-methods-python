dd66d84adf626dbda8f349e3d32b5d4984e7e013
Eden Iyanda <106480499+edeniyanda@users.noreply.github.com>
2025-05-09 16:53:46 +0100

    Implement sorting of the matrix in the Gaussian elimination solver

--- main.py
+++ main.py
@@ -20,9 +20,32 @@ def get_values():
 
     return matrix
 
+def sort_matrix(matrix):
+    x_max, x_max_row_index = matrix[0][0], 0
+    for i in range(len(matrix)):
+        if matrix[i][0] > x_max:
+            x_max = matrix[i][0]
+            x_max_row_index = i
+    # Swap the first row with the row containing the maximum element
+    matrix[0], matrix[x_max_row_index] = matrix[x_max_row_index], matrix[0]
+
+    y_max, y_max_row_index = matrix[1][1], 2
+    for i in range(1, len(matrix)):
+        if matrix[i][1] > y_max:
+            y_max = matrix[i][1]
+            y_max_row_index = i
+    # Swap the second row with the row containing the maximum element
+    matrix[1], matrix[y_max_row_index] = matrix[y_max_row_index], matrix[1]
+
+
+    return matrix
+
 
 if __name__ == "__main__":
     welcome()
     matrix = get_values()
     print(matrix)
+    sorted_natrix = sort_matrix(matrix)
+    for row in sorted_natrix:
+        print(row)
  
